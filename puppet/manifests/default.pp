6ac65e3dba0c02f3a7c879b2afb4a53dad110aba
Dylan Tack <git@dylan-tack.net>
2014-01-14 10:48:24 -0800

    create one database by default

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -10,6 +10,10 @@ node default {
   class { 'mysqld':
     password => 'root',
   }
+  mysqld::db::create { 'trevor':
+    user => 'trevor',
+    password => 'trevor',
+  }
   class { 'php53':
     memory_share => 0.65, # Percentage of RAM used for PHP
     worker_average_memory => 100, # estimate average PHP memory, in MB
