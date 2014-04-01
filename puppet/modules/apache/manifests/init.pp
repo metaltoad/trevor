12775588e26637453d4c78941d48e98e6badb627
Dylan Tack <git@dylan-tack.net>
2014-03-31 17:33:32 -0700

    Use ciphers reccomended by Mozilla (with priority for forward secrecy)

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -57,6 +57,23 @@ class apache::config {
       "set *[self::directive='EnableSendfile']/arg 'Off'",
     ],
   }
+
+  # Use ciphers reccomended by https://wiki.mozilla.org/Security/Server_Side_TLS
+  augeas { 'httpd-ssl':
+    context => '/files/etc/httpd/conf.d/ssl.conf/VirtualHost',
+    require => Package['httpd'],
+    notify => Service['httpd'],
+    changes => [
+      "set directive[. = 'SSLProtocol'] SSLProtocol",
+      "set *[self::directive='SSLProtocol']/arg 'all -SSLv2'",
+
+      "set directive[. = 'SSLHonorCipherOrder'] SSLHonorCipherOrder",
+      "set *[self::directive='SSLHonorCipherOrder']/arg 'on'",
+
+      "set directive[. = 'SSLCipherSuite'] SSLCipherSuite",
+      "set *[self::directive='SSLCipherSuite']/arg 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-RC4-SHA:ECDHE-ECDSA-RC4-SHA:AES128:AES256:RC4-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK'",
+    ],
+  }
 }
 
 class apache::service {
