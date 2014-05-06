a914d7b5be415e6ef18c6e58aeef63296b69398d
Dylan Tack <git@dylan-tack.net>
2014-05-06 14:02:36 -0700

    add support for 5.4

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -16,7 +16,8 @@ node default {
     user => 'trevor',
     password => 'trevor',
   }
-  class { 'php53':
+  class { 'php':
+    version => '5.4', # available versions: 5.3, 5.4, 5.5
     memory_share => 0.65, # Percentage of RAM used for PHP
     worker_average_memory => 200, # estimate average PHP memory, in MB
   }
---
+++ puppet/modules/php/manifests/init.pp
@@ -0,0 +1,81 @@
+class php::install {
+  package { [
+      "php${php::package}",
+      "php${php::package}-cli",
+      "php${php::package}-common",
+      "php${php::package}-fpm",
+      "php${php::package}-pecl-apc",
+      "php${php::package}-pecl-imagick",
+      "php${php::package}-pecl-memcached",
+      "php${php::package}-pdo",
+      "php${php::package}-mysql",
+      "php${php::package}-xml",
+      "php${php::package}-mbstring",
+      "php${php::package}-gd",
+      "php${php::package}-pear",
+      "php${php::package}-mcrypt",
+    ]:
+    ensure => present,
+    require => Yumrepo['ius'],
+  }
+}
+
+class php::configure {
+  file { '/etc/httpd/conf.d/php.conf':
+    source => 'puppet:///modules/php/php.conf',
+    owner => 'root',
+    group => 'root',
+    require => [Package['httpd'], Package["php${php::package}"]],
+    notify => Service['httpd'],
+  }
+
+  file { '/etc/php.d':
+    ensure => directory,
+    recurse => true,
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/php/php.d',
+    require => Package["php${php::package}-common"],
+    notify => Service['php-fpm'],
+  }
+
+  file { '/etc/php-fpm.d/www.conf':
+    ensure => present,
+    content => template('php/www.conf.erb'),
+    require => Package["php${php::package}-fpm"],
+    notify => Service['php-fpm'],
+  }
+
+  augeas { 'php.ini':
+    context => "/files/etc/php.ini/PHP",
+    require => Package["php${php::package}"],
+    notify => Service['php-fpm'],
+    changes => [
+      "set expose_php Off",
+      "set allow_url_fopen On",
+      "set display_errors On",
+      "set error_reporting 'E_ALL | E_STRICT'",
+      "set date.timezone 'America/Los_Angeles'",
+    ],
+  }
+}
+
+class php::service {
+  service { 'php-fpm':
+    enable => true,
+    ensure => running,
+    require => Package["php${php::package}-fpm"],
+  }
+}
+
+class php ($version = '54', $memory_share = 1.0, $worker_average_memory = 64) {
+  $package = $version ? {
+    '5.3' => '53u',
+    '5.4' => '54',
+    '5.5' => '55u',
+  }
+
+  class { 'php::install': }
+  class { 'php::configure': }
+  class { 'php::service': }
+}
