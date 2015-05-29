ba213e46b2bd46fc5a6a224d1d34a9f82d2227e8
Nathan Wilkerson <nathan@metaltoad.com>
2015-05-29 13:40:04 -0700

    force capistrano install to ignore new ruby requirement.

--- puppet/modules/capistrano/manifests/init.pp
+++ puppet/modules/capistrano/manifests/init.pp
@@ -1,11 +1,27 @@
 class capistrano {
+  
+package { [
+  'highline',
+  'net-ssh',
+  'net-sftp',
+  'net-scp',
+  'net-ssh-gateway'
+  ]:
+    provider => 'gem',
+    install_options => ["-f"],
+    notify => Package['capistrano'],
+  }
+
   package { 'capistrano':
     ensure => '2.15.5',
     provider => 'gem',
+    install_options => ["-f"],    
   }
+ 
   package { 'capistrano-ext':
     ensure => '1.2.1',
     provider => 'gem',
     require => Package['capistrano'],
+    install_options => ["-f"],
   }
 }
